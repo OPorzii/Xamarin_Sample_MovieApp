5a080d605fd396c72ca4f9a563ce83a14fd27399
OPorzii <59070103@it.kmitl.ac.th>
2019-07-24 11:42:38 +0700

    Add fetch api, detail page

---
+++ MovieSample/MovieSample/View/TestView1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MovieSample.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestView1 : ContentPage
+    {
+        public TestView1()
+        {
+            InitializeComponent();
+        }
+    }
+}
